0fe01efef62a1adc869b9a14356521f598cde3de
Althaafzz <althaaf.zaydaan@gmail.com>
2022-03-21 10:10:20 +0700

    finishing splash screen :partying_face:

--- app/src/main/java/com/auliamnaufal/sovone/SplashScreen.kt
+++ app/src/main/java/com/auliamnaufal/sovone/SplashScreen.kt
@@ -1,11 +1,13 @@
 package com.auliamnaufal.sovone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashScreen : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_splash_screen)
+		startActivity(Intent(this,MainActivity::class.java))
+		finish()
 	}
 }
